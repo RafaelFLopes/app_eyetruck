e06148f39c1c807b4e54a3532845bc1de37d749c
RafaelFLopes <ferreiralopesrafael109@gmail.com>
2025-08-16 20:28:16 -0300

    removendo o navigate EditarPerfil do arquivo home.tsx

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,16 +1,10 @@
-import { useNavigation } from '@react-navigation/native';
 import { Text, TouchableOpacity, View } from "react-native";
 
 export default function Home() {
 
-  const navigation = useNavigation(); //para conseguir navegar entre as telas
-
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('EditarPerfil')}>
-        <Text>Editar Perfil</Text>
-      </TouchableOpacity>
     </View>
   );
 }
